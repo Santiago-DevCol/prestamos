e36dc7ec592da9f3df2bd50e74f8fa6a6e289e99
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-03-09 23:37:43 -0500

    Mi primer servidor en php para mi primer programa

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+    const serverUrl="http://localhost/prestamos/";
+    const company= "Sistemas Prestamos SG";
+    const moneda= "$";
+
+    date_default_timezone_set("America/Bogota");
+?>
---
+++ config/server.php
@@ -0,0 +1,12 @@
+<?php
+    const server="localhost";
+    const DB="prestamos";
+    const user="root";
+    const pass="";
+
+    const SGBD="mysql:host=".server."; dbname=".DB;
+
+    const method="AES-256-CBC";
+    const secret_Key='$Prestamos@2023';
+    const secret_Iv='037970';
+?>
