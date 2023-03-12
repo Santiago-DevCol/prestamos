085ac855089796802a4db697d1701744b31cd819
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-03-11 21:53:06 -0500

    🌟 feat: creando y configurando la plantilla base

---
+++ vistas/inc/link.php
@@ -0,0 +1,23 @@
+	<!-- Normalize V8.0.1 -->
+<link rel="stylesheet" href="<?php echo serverUrl;?>vistas/css/normalize.css">
+
+	<!-- Bootstrap V4.3 -->
+<link rel="stylesheet" href="<?php echo serverUrl;?>vistas/css/bootstrap.min.css">
+
+	<!-- Bootstrap Material Design V4.0 -->
+<link rel="stylesheet" href="<?php echo serverUrl;?>vistas/css/bootstrap-material-design.min.css">
+
+	<!-- Font Awesome V5.9.0 -->
+<link rel="stylesheet" href="<?php echo serverUrl;?>vistas/css/all.css">
+
+	<!-- Sweet Alerts V8.13.0 CSS file -->
+<link rel="stylesheet" href="<?php echo serverUrl;?>vistas/css/sweetalert2.min.css">
+
+	<!-- Sweet Alert V8.13.0 JS file-->
+<script src="<?php echo serverUrl;?>vistas/js/sweetalert2.min.js" ></script>
+
+	<!-- jQuery Custom Content Scroller V3.1.5 -->
+<link rel="stylesheet" href="<?php echo serverUrl;?>vistas/css/jquery.mCustomScrollbar.css">
+	
+	<!-- General Styles -->
+<link rel="stylesheet" href="<?php echo serverUrl;?>vistas/css/style.css">
