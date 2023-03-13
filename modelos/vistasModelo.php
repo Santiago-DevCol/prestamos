cfc8eda19b8bac73a339f96ba3f556286807f028
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-03-12 20:39:28 -0500

    🌟 feat: creando la lista blanca de enlaces

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -3,7 +3,12 @@
     class vistasModelo{
         //MODELO a obtener las vistas
         protected static function obteber_vistas_modelo($vistas){
-            $listaBlanca = [];
+            $listaBlanca = ["home","client-new","client-list",
+                            "client-search","client-update","item-new",
+                            "item-list","item-search","item-update",
+                            "reservation-new","reservation-reservation","reservation-pending",
+                            "reservation-list","reservation-search","reservation-update",
+                            "user-new","user-list","user-search","user-update","company"];
             if(in_array($vistas,$listaBlanca)){
                 if(is_file("./vistas/contenidos/".$vistas."-view.php")){
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
