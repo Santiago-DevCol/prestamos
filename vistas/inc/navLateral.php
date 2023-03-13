4904d1d9bbc99624babe906e96e67e99a7faa6bd
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-03-12 21:23:42 -0500

    🐛 fix: Corrigiendo enlaces 1

--- vistas/inc/navLateral.php
+++ vistas/inc/navLateral.php
@@ -12,19 +12,19 @@
 		<nav class="full-box nav-lateral-menu">
 			<ul>
 				<li>
-					<a href="home.html"><i class="fab fa-dashcube fa-fw"></i> &nbsp; Dashboard</a>
+					<a href="<?php echo serverUrl;?>home/"><i class="fab fa-dashcube fa-fw"></i> &nbsp; Dashboard</a>
 				</li>
 				<li>
 					<a href="#" class="nav-btn-submenu"><i class="fas fa-users fa-fw"></i> &nbsp; Clientes <i class="fas fa-chevron-down"></i></a>
 					<ul>
 						<li>
-							<a href="client-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar Cliente</a>
+							<a href="<?php echo serverUrl;?>client-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar Cliente</a>
 						</li>
 						<li>
-							<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Lista de clientes</a>
+							<a href="<?php echo serverUrl;?>client-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Lista de clientes</a>
 						</li>
 						<li>
-							<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar cliente</a>
+							<a href="<?php echo serverUrl;?>client-search/"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar cliente</a>
 						</li>
 					</ul>
 				</li>
@@ -32,13 +32,13 @@
 					<a href="#" class="nav-btn-submenu"><i class="fas fa-pallet fa-fw"></i> &nbsp; Items <i class="fas fa-chevron-down"></i></a>
 					<ul>
 						<li>
-							<a href="item-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar item</a>
+							<a href="<?php echo serverUrl;?>item-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar item</a>
 						</li>
 						<li>
-							<a href="item-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Lista de items</a>
+							<a href="<?php echo serverUrl;?>item-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Lista de items</a>
 						</li>
 						<li>
-							<a href="item-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar item</a>
+							<a href="<?php echo serverUrl;?>item-search/"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar item</a>
 						</li>
 					</ul>
 				</li>
@@ -46,19 +46,19 @@
 					<a href="#" class="nav-btn-submenu"><i class="fas fa-file-invoice-dollar fa-fw"></i> &nbsp; Préstamos <i class="fas fa-chevron-down"></i></a>
 					<ul>
 						<li>
-							<a href="reservation-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Nuevo préstamo</a>
+							<a href="<?php echo serverUrl;?>reservation-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; Nuevo préstamo</a>
 						</li>
 						<li>
-							<a href="reservation-reservation.html"><i class="far fa-calendar-alt fa-fw"></i> &nbsp; Reservaciones</a>
+							<a href="<?php echo serverUrl;?>reservation-reservation/"><i class="far fa-calendar-alt fa-fw"></i> &nbsp; Reservaciones</a>
 						</li>
 						<li>
-							<a href="reservation-pending.html"><i class="fas fa-hand-holding-usd fa-fw"></i> &nbsp; Préstamos</a>
+							<a href="<?php echo serverUrl;?>reservation-pending/"><i class="fas fa-hand-holding-usd fa-fw"></i> &nbsp; Préstamos</a>
 						</li>
 						<li>
-							<a href="reservation-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Finalizados</a>
+							<a href="<?php echo serverUrl;?>reservation-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Finalizados</a>
 						</li>
 						<li>
-							<a href="reservation-search.html"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; Buscar por fecha</a>
+							<a href="<?php echo serverUrl;?>reservation-search/"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; Buscar por fecha</a>
 						</li>
 					</ul>
 				</li>
@@ -66,18 +66,18 @@
 					<a href="#" class="nav-btn-submenu"><i class="fas  fa-user-secret fa-fw"></i> &nbsp; Usuarios <i class="fas fa-chevron-down"></i></a>
 					<ul>
 						<li>
-							<a href="user-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Nuevo usuario</a>
+							<a href="<?php echo serverUrl;?>user-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; Nuevo usuario</a>
 						</li>
 						<li>
-							<a href="user-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Lista de usuarios</a>
+							<a href="<?php echo serverUrl;?>user-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Lista de usuarios</a>
 						</li>
 						<li>
-							<a href="user-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar usuario</a>
+							<a href="<?php echo serverUrl;?>user-search/"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar usuario</a>
 						</li>
 					</ul>
 				</li>
 				<li>
-					<a href="company.html"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Empresa</a>
+					<a href="<?php echo serverUrl;?>company/"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Empresa</a>
 				</li>
 			</ul>
 		</nav>
